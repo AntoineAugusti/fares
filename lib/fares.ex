90045b94dc2c03562f7d4c2eafc261be84e3b53f
Antoine Augusti <antoine.augusti@gmail.com>
2022-03-05 16:39:25 +0100

    Add NightTrip

--- lib/fares.ex
+++ lib/fares.ex
@@ -1,49 +1,72 @@
 defmodule FareRule do
   defguard positive_int(x) when is_integer(x) and x > 0
+  defguard positive_number(x) when is_number(x) and x > 0
   @callback can_apply?(any(), DateTime.t()) :: boolean
   @callback run(any(), [DateTime.t()], integer) :: {integer, [DateTime.t()]}
+
+  def run_for_minutes(_, [], cost), do: {cost, []}
+
+  def run_for_minutes(%{minutes: minutes, cost: rule_cost}, [head | tail], cost) do
+    datetime_after = DateTime.add(head, minutes * 60, :second)
+    {cost + rule_cost, tail |> Enum.filter(&(DateTime.compare(&1, datetime_after) == :gt))}
+  end
 end
 
 defmodule SingleTrip do
   @enforce_keys [:trip_duration_minutes, :cost]
   defstruct [:trip_duration_minutes, :cost]
-  import FareRule, only: [positive_int: 1]
+  import FareRule
   @behaviour FareRule
 
   def new(trip_duration_minutes, cost)
-      when positive_int(trip_duration_minutes) and positive_int(cost) do
+      when positive_int(trip_duration_minutes) and positive_number(cost) do
     %__MODULE__{trip_duration_minutes: trip_duration_minutes, cost: cost}
   end
 
   def can_apply?(%__MODULE__{}, _), do: true
+
+  def run(%__MODULE__{trip_duration_minutes: minutes, cost: rule_cost}, trips, cost) do
+    run_for_minutes(%{minutes: minutes, cost: rule_cost}, trips, cost)
+  end
+end
+
+defmodule NightTrip do
+  @enforce_keys [:hour_start, :next_day_hour_end, :cost]
+  defstruct [:hour_start, :next_day_hour_end, :cost]
+  import FareRule
+  @behaviour FareRule
+  def new(hour_start, next_day_hour_end, cost)
+      when positive_int(hour_start) and positive_int(next_day_hour_end) and positive_number(cost) and
+             hour_start <= 24 and next_day_hour_end <= 24 do
+    %__MODULE__{hour_start: hour_start, next_day_hour_end: next_day_hour_end, cost: cost}
+  end
+
+  def can_apply?(%__MODULE__{hour_start: hour_start}, %DateTime{hour: hour}) do
+    hour >= hour_start
+  end
+
   def run(%__MODULE__{}, [], cost), do: {cost, []}
 
-  def run(
-        %__MODULE__{trip_duration_minutes: trip_duration_minutes, cost: rule_cost},
-        [head | tail],
-        cost
-      ) do
-    datetime_after = DateTime.add(head, trip_duration_minutes * 60, :second)
-    {cost + rule_cost, tail |> Enum.filter(&(DateTime.compare(&1, datetime_after) == :gt))}
+  def run(%__MODULE__{next_day_hour_end: hour_end, cost: rule_cost}, [head | tail], cost) do
+    datetime_end = %{DateTime.add(head, 60 * 60 * 24, :second) | hour: hour_end}
+    {cost + rule_cost, tail |> Enum.filter(&(DateTime.compare(&1, datetime_end) == :gt))}
   end
 end
 
 defmodule ConsecutiveDays do
   @enforce_keys [:nb_days, :cost]
   defstruct [:nb_days, :cost]
-  import FareRule, only: [positive_int: 1]
+  import FareRule
   @behaviour FareRule
 
-  def new(nb_days, cost) when positive_int(nb_days) and positive_int(cost) do
+  def new(nb_days, cost) when positive_int(nb_days) and positive_number(cost) do
     %__MODULE__{nb_days: nb_days, cost: cost}
   end
 
   def can_apply?(%__MODULE__{}, _), do: true
-  def run(%__MODULE__{}, [], cost), do: {cost, []}
 
-  def run(%__MODULE__{nb_days: nb_days, cost: rule_cost}, [head | tail], cost) do
-    datetime_after = DateTime.add(head, nb_days * 60 * 60 * 24, :second)
-    {cost + rule_cost, tail |> Enum.filter(&(DateTime.compare(&1, datetime_after) == :gt))}
+  def run(%__MODULE__{nb_days: nb_days, cost: rule_cost}, trips, cost) do
+    run_for_minutes(%{minutes: nb_days * 60 * 24, cost: rule_cost}, trips, cost)
   end
 end
 
--- test/fares_test.exs
+++ test/fares_test.exs
@@ -37,6 +37,19 @@ defmodule FaresTest do
                ~U[2018-11-02 10:06:00Z]
              ]
            ) == 7
+
+    assert Fares.compute(
+             [SingleTrip.new(60, 2), NightTrip.new(19, 6, 1.5)],
+             [
+               ~U[2018-11-01 10:05:00Z],
+               ~U[2018-11-01 12:05:00Z],
+               ~U[2018-11-01 16:05:00Z],
+               ~U[2018-11-02 19:06:00Z],
+               ~U[2018-11-02 21:06:00Z],
+               ~U[2018-11-03 02:30:00Z],
+               ~U[2018-11-03 09:30:00Z]
+             ]
+           ) == 9.5
   end
 
   describe "SingleTrip" do
@@ -48,7 +61,7 @@ defmodule FaresTest do
       assert SingleTrip.can_apply?(SingleTrip.new(60, 1), ~U[2018-11-15 10:00:00Z])
     end
 
-    test "apply" do
+    test "run" do
       assert {1, []} == SingleTrip.run(SingleTrip.new(60, 1), [~U[2018-11-15 10:00:00Z]], 0)
 
       assert {1, []} ==
@@ -76,7 +89,7 @@ defmodule FaresTest do
       assert ConsecutiveDays.can_apply?(ConsecutiveDays.new(1, 10), ~U[2018-11-15 10:00:00Z])
     end
 
-    test "apply" do
+    test "run" do
       assert {15, [~U[2018-11-16 10:05:00Z]]} ==
                ConsecutiveDays.run(
                  ConsecutiveDays.new(1, 10),
@@ -99,6 +112,36 @@ defmodule FaresTest do
                )
 
       assert {5, []} == ConsecutiveDays.run(ConsecutiveDays.new(5, 10), [], 5)
+    end
+  end
+
+  describe "NightTrip" do
+    test "new" do
+      NightTrip.new(19, 6, 5)
+    end
+
+    test "can_apply?" do
+      assert NightTrip.can_apply?(NightTrip.new(19, 6, 5), ~U[2018-11-15 19:05:00Z])
+      assert NightTrip.can_apply?(NightTrip.new(19, 6, 5), ~U[2018-11-15 23:50:00Z])
+      refute NightTrip.can_apply?(NightTrip.new(19, 6, 5), ~U[2018-11-15 18:55:00Z])
+    end
+
+    test "run" do
+      assert {2, []} == NightTrip.run(NightTrip.new(19, 6, 2), [~U[2018-11-15 19:05:00Z], ~U[2018-11-15 21:05:00Z]], 0)
+
+      assert {2, []} ==
+               NightTrip.run(
+                 NightTrip.new(19, 6, 2),
+                 [~U[2018-11-15 19:05:00Z], ~U[2018-11-15 21:05:00Z], ~U[2018-11-16 05:55:00Z]],
+                 0
+               )
+
+      assert {3, [~U[2018-11-16 06:30:00Z]]} ==
+               NightTrip.run(
+                 NightTrip.new(19, 6, 2),
+                 [~U[2018-11-15 19:05:00Z], ~U[2018-11-15 21:05:00Z], ~U[2018-11-16 06:30:00Z]],
+                 1
+               )
     end
   end
 end
